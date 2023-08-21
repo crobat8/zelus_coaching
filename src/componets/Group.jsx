0af307f4b0d129f3301112c2f50d792818c25b56
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-08-20 17:49:34 -0700

    can add individuel athletes to event groups

--- src/componets/Group.jsx
+++ src/componets/Group.jsx
@@ -1,14 +1,37 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AthleteContext } from "../context/AthletesContext";
 import { AuthContext } from "../context/AuthContext";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { arrayUnion, collection, doc, onSnapshot, query, updateDoc, where } from "firebase/firestore";
 import { db } from "../firebase";
+import AthleteList from "./AthleteList";
 
 const Group = (props) =>{
   const{athletesNames} = useContext(AthleteContext);
   const{currentUser} = useContext(AuthContext);
   const[groupList,setGroupList]=useState([])
   console.log(props.title)
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    console.log(e.target[0].value)
+    var groupSpot = currentUser.uid+"_"+props.title
+    const groupsRef = doc(db,"groups",groupSpot)
+    updateDoc(groupsRef,{
+      groupUIDS: arrayUnion(e.target[0].value)
+    }).then(()=>{
+      alert("sucessfully added athlete to group")
+    })
+  }
+
+  function convertUID(x){
+    console.log(athletesNames)
+    for(var i = 0; i<athletesNames.length; i++){
+      if(athletesNames[i].uid === x){
+        return athletesNames[i].displayName
+      }
+    }
+    return x
+  }
 
   useEffect(() => {
     var groupSpot = currentUser.uid+"_"+props.title
@@ -24,15 +47,7 @@ const Group = (props) =>{
 
   }, [props]);
 
-  function convertUID(x){
-    console.log(athletesNames)
-    for(var i = 0; i<athletesNames.length; i++){
-      if(athletesNames[i].uid === x){
-        return athletesNames[i].displayName
-      }
-    }
-    return x
-  }
+
 
   if(!groupList[0]){
     console.log(groupList)
@@ -50,6 +65,16 @@ const Group = (props) =>{
       <h1>
         {props.title}
       </h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          add an athlete to this event group
+        </p>
+        <AthleteList/>
+        <button>
+          add athlete
+        </button>
+      </form>
+      
       {groupList[0].groupUIDS.map((e)=>{
         var tempName = convertUID(e)
         
