78f650c3f5aac1d592850bd2e433bfee02527650
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-08-20 23:35:27 -0700

    added remove athlete button

--- src/componets/MyAthletes.jsx
+++ src/componets/MyAthletes.jsx
@@ -2,11 +2,29 @@ import React, { useContext, useState,useEffect } from "react";
 import { UserContext } from "../context/UserContext";
 import { AuthContext } from "../context/AuthContext";
 import { AthleteContext } from "../context/AthletesContext";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 const MyAthlete = () =>{
   const{userInfo} =useContext(UserContext)
   const{athletesNames} = useContext(AthleteContext)
   const{currentUser} = useContext(AuthContext);
   
+  function removeAthlete (x){
+    console.log(x)
+    const coachRef = doc(db,"users",currentUser.uid)
+    updateDoc(coachRef,{
+      athletes: arrayRemove(x.uid)
+    })
+
+    const athleteRef = doc(db,"users",x.uid);
+    updateDoc(athleteRef,{
+      coaches: arrayRemove(currentUser.uid),
+    })
+
+    alert("sucessfully removed athlete");
+  }
+
+
   return(
     <div>
       <h1>
@@ -14,9 +32,15 @@ const MyAthlete = () =>{
       </h1>
       {athletesNames.map((e,key)=>{
         return(
-          <h2 key={key}>
-            {e.displayName}
-          </h2>
+          <div>
+            <h2 key={key}>
+              {e.displayName}
+            </h2>
+            <button onClick={()=>removeAthlete(e)}>
+              remove athlete
+            </button>
+          </div>
+
         )
       })}
     </div>
